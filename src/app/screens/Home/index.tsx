3a51e7d141f23bbcaeebd12d78f98afbb0025f8b
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2024-08-29 01:52:10 +0100

    cursorBlue

--- src/app/screens/Home/index.tsx
+++ src/app/screens/Home/index.tsx
@@ -1,13 +1,16 @@
 import styles from "./index.module.css";
 import Navbar from "@/app/components/Navbar";
-
+import CircleAnimation from "@/app/components/Cursor"
+import Image from "next/image";
+import robot from '../../assets/ROBOT.png';
 export default function Home() {
   return (
     
       <div className={styles.container}>
     
+      <CircleAnimation/>
       <Navbar/>
-      
+     
       
      
     
